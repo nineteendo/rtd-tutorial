1f2cb7f5b82b1d843c63a91d48e050b09e07174d
Wannes Boeykens <wannes.boeykens@student.kuleuven.be>
2025-03-28 08:31:08 +0100

    Simplify example

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,14 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
 
-# -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
-
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
